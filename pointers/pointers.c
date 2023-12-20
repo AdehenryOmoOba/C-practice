e5919dab40dff16d83e7a0c4a6218df6ef3e56ba
adehenry <adefemiadelaja@gmail.com>
2023-12-20 14:09:12 +0100

    pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -2,30 +2,52 @@
 
 int main()
 {
-    int age = 27;
+    // int age = 27;
 
-    int *agePtr = &age;
+    // int *agePtr = &age;
 
-    int **agePtrPtr = &agePtr;
+    // int **agePtrPtr = &agePtr;
 
-    int ***agePtrPtrPtr = &agePtrPtr;
+    // int ***agePtrPtrPtr = &agePtrPtr;
 
-    printf("Age value is:                %d\n", age);
-    printf("Age mem address is:          %p\n", &age);
+    // printf("Age value is:                %d\n", age);
+    // printf("Age mem address is:          %p\n", &age);
 
-    printf("agePtr value is:             %p\n", agePtr);
-    printf("agePtr mem address is:       %p\n", &agePtr);
+    // printf("agePtr value is:             %p\n", agePtr);
+    // printf("agePtr mem address is:       %p\n", &agePtr);
 
-    printf("agePtrPtr value is:          %p\n", agePtrPtr);
-    printf("agePtrPtr mem address is:    %p\n", &agePtrPtr);
+    // printf("agePtrPtr value is:          %p\n", agePtrPtr);
+    // printf("agePtrPtr mem address is:    %p\n", &agePtrPtr);
 
-    printf("agePtrPtrPtr value is:       %p\n", agePtrPtrPtr);
-    printf("agePtrPtrPtr mem address is: %p\n", &agePtrPtrPtr);
+    // printf("agePtrPtrPtr value is:       %p\n", agePtrPtrPtr);
+    // printf("agePtrPtrPtr mem address is: %p\n", &agePtrPtrPtr);
 
-    printf("\n");
+    // printf("\n");
 
-    printf("access 'age' value via variable 'age':         %d\n", age);
-    printf("access 'age' value via pointer 'agePtr':       %d\n", *agePtr);
-    printf("access 'age' value via pointer 'agePtrPtr':    %d\n", **agePtrPtr);
-    printf("access 'age' value via pointer 'agePtrPtrPtr': %d\n", ***agePtrPtrPtr);
+    // printf("access 'age' value via variable 'age':         %d\n", age);
+    // printf("access 'age' value via pointer 'agePtr':       %d\n", *agePtr);
+    // printf("access 'age' value via pointer 'agePtrPtr':    %d\n", **agePtrPtr);
+    // printf("access 'age' value via pointer 'agePtrPtrPtr': %d\n", ***agePtrPtrPtr);
+
+    // Using void pointers:
+    // int age = 14;
+    // float height = 7.20;
+    // char initial = 'z';
+
+    // void *myPointer;
+
+    // myPointer = &age;
+    // printf("myPointer used as int pointer: %d\n", *(int *)myPointer);
+
+    // myPointer = &height;
+    // printf("myPointer used as float pointer: %.2f\n", *(float *)myPointer);
+
+    // myPointer = &initial;
+    // printf("myPointer used as char pointer: %c\n", *(char *)myPointer);
+
+    // Null pointers:
+    // int *myPointer = NULL;
+    // int *myPointer = 0;
+
+    // Dangling pointers:
 }
